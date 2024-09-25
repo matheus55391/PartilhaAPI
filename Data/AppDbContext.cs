1872b601b9242d233563d18410771e4e3111fdd4
matheus55391 <matheus.felipe55391@gmail.com>
2024-09-24 23:02:37 -0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // Defina suas entidades aqui, por exemplo:
+    // public DbSet<Expense> Expenses { get; set; }
+}
